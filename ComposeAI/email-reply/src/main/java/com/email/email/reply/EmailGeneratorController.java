e3b55ec489a0f45fa427327f7d696a796a12373d
Shivansh TIwari <shivanshtiwari3421@icloud.com>
2025-08-31 21:49:26 +0530

    Add files via upload

---
+++ ComposeAI/email-reply/src/main/java/com/email/email/reply/EmailGeneratorController.java
@@ -0,0 +1,24 @@
+package com.email.email.reply;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+@CrossOrigin(
+        origins = "*",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS}
+)
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
